0bdc3005e9f000fe3fc0bc199e79fe8d8ca22d3e
Noobieprogrammer69 <joververgara1@gmail.com>
2022-02-21 15:21:41 +0800

    more cahnges

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,20 +2,18 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 
-import { getRecentPosts, getSimilarPosts } from '../services';
+import { getCategoryPost, getRecentPosts, getSimilarPosts } from '../services';
 
 const PostWidget = ({ categories, slug }) => {
     const [relatedPosts, setRelatedPosts] = useState([]);
   
     useEffect(() => {
       if (slug) {
-        getSimilarPosts(categories, slug).then((result) => {
-          setRelatedPosts(result);
-        });
+        getSimilarPosts(category, slug)
+            .then((result) => setRelatedPosts(result))
       } else {
-        getRecentPosts().then((result) => {
-          setRelatedPosts(result);
-        });
+        getRecentPosts()
+            .then((result) => setRelatedPosts(result)) 
       }
     }, [slug]);
   
